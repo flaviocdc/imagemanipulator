ae2d4d559d7bbace2e63e7829bf3c9a0c67757e2
Flavio Costa <flavio.cdc@gmail.com>
2011-09-04 04:08:11 -0300

    Fixing typo

--- src/main/java/br/ufrj/dcc/compgraf/im/ui/options/GreyScaleOptionsDialog.java
+++ src/main/java/br/ufrj/dcc/compgraf/im/ui/options/GreyScaleOptionsDialog.java
@@ -58,7 +58,7 @@ public class GreyScaleOptionsDialog extends JDialog
       }
     });
     
-    add(new JLabel("Type"));
+    add(new JLabel("Type:"));
     add(typesCombo);
     add(applyButton);
   }
